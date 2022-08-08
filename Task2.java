6e5a72121df6600e4e9506498cb2f576ea8fb019
Anish218 <anishmishram218@gmail.com>
2022-08-08 18:13:27 +0530

    first commit

--- Task2.java
+++ Task2.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Task2 {
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String []args)
+    {
+        Scanner myObj = new Scanner(System.in);
+        int firstNumber=myObj.nextInt();
+        int currentNumber=firstNumber;
+        while(currentNumber!=1)
+        {
+            System.out.print(currentNumber+" ");
+            if((currentNumber | 1) ==currentNumber+1)
+            {
+                currentNumber/=2;
+            }
+            else
+                currentNumber=3*currentNumber+1;
+
+        }
+        System.out.println(1);
+
+
+    }
 }
