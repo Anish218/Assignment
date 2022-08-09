9b3d159456196cd9cac68d11668a5f48856230c1
Anish218 <anishmishram218@gmail.com>
2022-08-09 13:20:05 +0530

    mode found

--- Day2/src/ArrayOperation.java
+++ Day2/src/ArrayOperation.java
@@ -7,9 +7,11 @@ public class ArrayOperation {
         int sizeofarray=sc.nextInt();
         int elements[]=new int[sizeofarray];
         int mean=0;
+        int count[]=new int[sizeofarray+1];
         for(int i=0;i<sizeofarray;i++)
         {
             elements[i]=sc.nextInt();
+            count[elements[i]]+=1;
             mean+=elements[i];
 
         }
@@ -24,6 +26,14 @@ public class ArrayOperation {
         }
         else
             System.out.println("median of array is "+ (double)(elements[sizeofarray/2]));
-
+            int maximumcount=0;
+            int indexmaximumelement=0;
+        for(int i=0;i<count.length;i++)
+        {
+            if(count[i]>maximumcount){
+                indexmaximumelement=i;
+            maximumcount=count[i];}
+        }
+        System.out.println("element with maximum count " +indexmaximumelement);
     }
 }
