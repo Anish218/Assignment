3b6d2bcd0cd99b5e7931d36d182ac463b9fb7d3d
Anish218 <anishmishram218@gmail.com>
2022-08-09 16:28:31 +0530

    created class and added constructor

---
+++ Day2/src/OopsExample.java
@@ -0,0 +1,19 @@
+class Holiday {
+    private String name;
+    private int day;
+    private String month;
+    Holiday(String name,int day,String month)
+    {
+        this.day=day;
+        this.month=month;
+        this.name=name;
+    }
+
+
+}
+
+public class OopsExample {
+    public static void main(String[] args) {
+
+    }
+}
