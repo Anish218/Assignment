3eec208c4cbd206ddab1cf2351ec625bc6321919
Anish218 <anishmishram218@gmail.com>
2022-08-09 16:43:51 +0530

    created a menthod avgDate

--- Day2/src/OopsExample.java
+++ Day2/src/OopsExample.java
@@ -8,9 +8,18 @@ class Holiday {
         this.month=month;
         this.name=name;
     }
-    public boolean inSameMonth(String month1,String month2)
+    public static  boolean inSameMonth(Holiday holiday1,Holiday holiday2)
     {
-        return (month1.equals(month2));
+        return (holiday1.month.equals(holiday2.month));
+    }
+    public static double avgDate(Holiday array[])
+    {
+        int avg=0;
+        for(int i=0;i<array.length;i++)
+        {
+            avg+=array[i].day;
+        }
+        return (double) avg/ array.length;
     }
 
 
@@ -18,6 +27,15 @@ class Holiday {
 
 public class OopsExample {
     public static void main(String[] args) {
+        Holiday obj1=new Holiday("Independance day",15,"August");
+        Holiday obj2=new Holiday("Republic day",26,"January");
+        Holiday array[];
+        array=new Holiday[2];
+        array[0]=obj1;
+        array[1]=obj2;
+        System.out.println(Holiday.avgDate(array));
+        System.out.println(Holiday.inSameMonth(obj1,obj2));
+
 
     }
 }
